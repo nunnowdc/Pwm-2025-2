a55a5fa03718aa6da0f86ab5e5aa6891ce02c65f
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-08-15 12:03:43 +0000

    Mudança no codigo

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -4,7 +4,13 @@
 export default function Home() {
   return (
     <div>
-      <h1> aaa </h1>
+      <h1> Programação Web Mobile 25.2 </h1>
+      <br />
+      <hr></hr>
+      <br />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea dolor eligendi rem cumque perferendis ratione sint hic reprehenderit cum. Tempora dolores commodi illum eaque mollitia error pariatur cum reprehenderit at! Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis numquam voluptatem culpa aspernatur delectus, iure sint magni adipisci enim. Maiores hic nobis quibusdam magnam officiis molestias nam atque obcaecati esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio excepturi, rerum unde eum id sequi esse ut nemo sit a laboriosam aspernatur debitis eaque iste, repellat non! Iusto, ea quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veniam neque delectus obcaecati, maxime excepturi odit animi iure laboriosam hic sequi in facere modi, commodi sed! Recusandae totam placeat maxime. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum repellat esse, facilis, veniam laudantium, amet iste quam rem ullam perferendis laborum? Autem eligendi eos voluptas doloremque similique odio? Aut, sint?</p>     
+      <br />
+      <a href="https://www.google.com/?hl=pt_BR">link para o google</a>
     </div>
   );
 }
