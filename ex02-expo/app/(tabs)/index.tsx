7ddca37c00e2584a67de2742c34b365914421447
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-10-07 01:05:25 +0000

    mini bio

--- ex02-expo/app/(tabs)/index.tsx
+++ ex02-expo/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -8,9 +8,23 @@ export default function HomeScreen() {
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "beige",
+        padding: 20, 
       }}
     >
-      <Text>Hello World!</Text>
+      <Image
+        source={require('../../assets/images/foto de perfil.jpg')}
+        style={{ width: 200, height: 200, marginBottom: 20 }}
+      />
+      <Text style={{ fontWeight: 'bold' }}>Nunno Wakiyama Diniz Carvalho</Text>
+      <Text style={{ fontWeight: 'bold' }}>RA: 00000849115</Text>
+
+      <Text style={{ marginTop: 20, textAlign: 'justify' }}> Sou estudante de Ciência da Computação, atualmente cursando o 5º período, com grande interesse em ingressar no mercado de trabalho e adquirir experiência prática na área de tecnologia. No último ano, participei de um projeto de Iniciação Científica (PIBIC - voluntário), onde fui responsável pelo treinamento de uma rede neural para detecção de patologias em edificações, experiência que me proporcionou contato direto com inteligência artificial e aprendizado de máquina. </Text>
+
+      <Text style={{ marginTop: 10, textAlign: 'justify' }}> Com o término desse projeto, estou em busca de novas oportunidades que me permitam transformar minhas habilidades em resultados reais, gerar renda e impulsionar minha carreira profissional. Sou comunicativo, proativo, tenho facilidade em trabalhar em equipe e me comprometo com dedicação e esforço em tudo o que faço. </Text>
+
+      <Text style={{ fontStyle: 'italic', marginTop: 20 }}>nunnowdcarvalho@gmail.com</Text>
+      <Text style={{ fontStyle: 'italic', marginTop: 5 }}>LinkedIn: nunnowakiyama</Text>
+      <Text style={{ fontStyle: 'italic', marginTop: 5 }}>Github: nunnowdc</Text>
     </View>
   );
 }
